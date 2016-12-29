c78ba7e1480dbf1bdf544478809d2f2cf8f42122
RATAM <ratamranjith@gmail.com>
2016-12-29 16:53:17 +0530

    Metaprograming in Ruby - Day1

---
+++ FisrtClass.rb
@@ -0,0 +1,6 @@
+# Class With out any Method  Declaration and Definition
+3.times do 
+	class FisrtClass
+		puts "Hello Ratam"
+	end
+end
---
+++ SecondClass.rb
@@ -0,0 +1,13 @@
+# Class 
+class SecondClass
+	def first_method
+		p "First Method is Executed"
+	end
+	def second_method
+		p "Second Method is Executed"
+	end
+
+end
+obj = SecondClass.new
+obj.first_method
+obj.second_method
